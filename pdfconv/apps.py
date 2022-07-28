82427da1e7742fcc6050d65c52755411196da726
Lakshmi <lakshmi@gmail.com>
2022-07-28 18:12:36 +0530

    Revert "2v"
    This reverts commit 14a71c60c7f8df41d5ae42a0748e5b453ff8653f.

--- pdfconv/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PdfconvConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pdfconv'
--- pdfconv/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('pdfconv/',views.about,name='about')
-]
